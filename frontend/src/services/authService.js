b6386f6000bafc64b7051fa58196d9c022530a04
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-07-30 11:45:45 -0700

    fix: API URL

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL;
 
 export async function loginUser(email, password) {
-    const response = await axios.post(`${API_URL}/login`, {
+    const response = await axios.post(`${API_URL}/auth/login`, {
         email,
         password,
     });
@@ -12,7 +12,7 @@ export async function loginUser(email, password) {
 }
 
 export async function registerUser(name, email, password) {
-    const response = await axios.post(`${API_URL}/register`, {
+    const response = await axios.post(`${API_URL}/auth/register`, {
         name,
         email,
         password,
--- frontend/src/services/reportService.js
+++ frontend/src/services/reportService.js
@@ -3,7 +3,7 @@ const API_URL = `${import.meta.env.VITE_API_URL}/report`;
 
 export async function getWeeklyReport() {
   const token = localStorage.getItem('token');
-  const response = await axios.get(`${API_URL}/weekly`, {
+  const response = await axios.get(`${API_URL}/report/weekly`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data; 
